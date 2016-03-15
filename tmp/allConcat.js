c79fb7270d61ed48f6cc90b814f4c475c5b576cd
Andrew Cho and Alex Larson <cho-andrew@hotmail.com>
2016-03-15 10:56:18 -0700

    some functionality working

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -2,10 +2,29 @@ var Alarm = require('./../js/alarm.js').Alarm;
 
 
 
-$(document).ready(function update_time() {
 
+$(document).ready(function() {
 
-    $('#time').text(moment());
+  $(document).ready(function update_time() {
+
+    $('#time').text(moment().format("h:m:ss"));
     setTimeout(update_time, 1000);
-  
+  });
+
+    var consoleTime = $('#time').text(moment().format("h:m:ss"));
+    console.log(consoleTime);
+
+    var newAlarmTime = "blank Time";
+
+    $(".alarmSet").submit(function () {
+      event.preventDefault();
+      newAlarmTime = $(".alarmTime").val();
+      console.log(newAlarmTime);
+    $(".printOut").append("<li> Your alarm is set for"+newAlarmTime+"</li>");
+    });
+
+    var currentTime = $("#time").text();
+    if (newAlarmTime === currentTime) {
+      alert("hey wake the heck up");
+    }
 });
