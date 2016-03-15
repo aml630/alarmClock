8b526e7be56c8be303c0b35d7b576c581918dad2
Andrew Cho and Alex Larson <cho-andrew@hotmail.com>
2016-03-15 11:28:17 -0700

    add alarm functionality

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -6,25 +6,35 @@ var Alarm = require('./../js/alarm.js').Alarm;
 $(document).ready(function() {
 
   $(document).ready(function update_time() {
-
-    $('#time').text(moment().format("h:m:ss"));
+    $('#time').text(moment().format("h:mm"));
     setTimeout(update_time, 1000);
+
+
+
+    if (newAlarmTime === consoleTime.text()) {
+      alert("hey wake the heck up");
+    }
   });
 
-    var consoleTime = $('#time').text(moment().format("h:m:ss"));
-    console.log(consoleTime);
+  var consoleTime = $('#time').text(moment().format("h:m"));
 
-    var newAlarmTime = "blank Time";
+  var newAlarmTime = "blank Time";
+
 
     $(".alarmSet").submit(function () {
       event.preventDefault();
       newAlarmTime = $(".alarmTime").val();
       console.log(newAlarmTime);
-    $(".printOut").append("<li> Your alarm is set for"+newAlarmTime+"</li>");
+      console.log(consoleTime.text());
+    $(".printOut").append("<li> Your alarm is set for "+newAlarmTime+"</li>");
+    //
+    // if (newAlarmTime === consoleTime.text()) {
+    //   alert("hey wake the heck up");
+    // }
     });
 
-    var currentTime = $("#time").text();
-    if (newAlarmTime === currentTime) {
-      alert("hey wake the heck up");
-    }
+
+    // if (newAlarmTime === consoleTime.text()) {
+    //   alert("hey wake the heck up");
+    // }
 });
